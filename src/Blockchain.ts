d887cb775286445faa6e24b1c3b433ddcbed39c3
David GAILLETON <david.gailleton@posteo.net>
2025-04-29 11:04:46 +0200

    Blockchain class replaceChain and isValideChain

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -1,4 +1,5 @@
 import Block from './Block';
+import sha256 from 'crypto-js/sha256'
 
 class Blockchain {
     public chain: Block[];
@@ -10,6 +11,43 @@ class Blockchain {
 
     addBlock(block: Block): void {
         this.chain.push(block);
+    }
+
+    /*static blockHash(block: Block): string {
+        const { lastHash, timestamp, data } = block;
+        return sha256(sha256(lastHash + timestamp.toString())).toString();
+    }*/
+
+    static blockHash(block: Block, lastBlock: Block) {
+        const calculatedHash = sha256(sha256(lastBlock.hash + lastBlock.timestamp.toString())).toString();
+        return calculatedHash;
+    }
+
+    static isValidChain(chain: Block[]): boolean {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        }
+
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+
+            if (block.lastHash !== lastBlock.hash) {
+                return false;
+            }
+
+            if (block.hash !== this.blockHash(block, lastBlock)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    replaceChain(chain: Block[]): void {
+        if (Blockchain.isValidChain(chain) && chain.length > this.chain.length) {
+            this.chain = chain;
+        }
     }
 }
 
--- src/blockchain.test.ts
+++ src/blockchain.test.ts
@@ -3,9 +3,11 @@ import Block from './Block';
 
 describe("Blockchain", () => {
     let blockchain: Blockchain;
+    let blockchain2: Blockchain;
 
     beforeEach(() => {
         blockchain = new Blockchain();
+        blockchain2 = new Blockchain();
     });
 
     it("starts with genesis block", () => {
@@ -19,7 +21,55 @@ describe("Blockchain", () => {
 
         blockchain.addBlock(newBlock);
 
-        expect(blockchain.chain[blockchain.chain.length - 1]).toEqual(newBlock);
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+    });
+
+    it("validates a valid chain", () => {
+        const data = 'foo';
+        const lastBlock = blockchain2.chain[blockchain2.chain.length - 1];
+        const newBlock = Block.mineBlock(lastBlock, data);
+
+        blockchain2.addBlock(newBlock);
+
+        expect(Blockchain.isValidChain(blockchain2.chain)).toBe(true);
+    });
+
+    it("invalidates a corrupted genesis block", () => {
+        blockchain2.chain[0].data = 'corrupted';
+        expect(Blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it("invalidates a corrupt chain", () => {
+        const data = 'foo';
+        const lastBlock = blockchain2.chain[blockchain2.chain.length - 1];
+        const newBlock = Block.mineBlock(lastBlock, data);
+        const corruptedBlock = new Block(1231006505000, "0000000000000000000000000000000000000000000000000000000000000000", "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f", "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks")
+
+        blockchain2.addBlock(newBlock);
+        blockchain2.chain[1] = corruptedBlock
+
+        expect(Blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it("replaces the chain with a valid chain", () => {
+        const data = 'goo';
+        const lastBlock = blockchain2.chain[blockchain2.chain.length - 1];
+        const newBlock = Block.mineBlock(lastBlock, data);
+
+        blockchain2.addBlock(newBlock);
+        blockchain.replaceChain(blockchain2.chain);
+
+        expect(blockchain.chain).toEqual(blockchain2.chain);
+    });
+
+    it("does not replace the chain with less than or equal chain", () => {
+        const data = 'foo';
+        const lastBlock = blockchain.chain[blockchain.chain.length - 1];
+        const newBlock = Block.mineBlock(lastBlock, data);
+
+        blockchain.addBlock(newBlock);
+        blockchain.replaceChain(blockchain2.chain);
+
+        expect(blockchain.chain).not.toEqual(blockchain2.chain);
     });
 });
