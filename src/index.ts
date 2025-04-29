aa24d527d846efcff9c35f5fdff508737fb3fba0
David GAILLETON <david.gailleton@posteo.net>
2025-04-29 11:33:35 +0200

    express API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Happy developing ✨')
+import Blockchain from "./class/Blockchain";
+import express from 'express';
+import Block from "./class/Block";
+
+const blockchain = new Blockchain();
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/blocks", (request, response) => {
+    response.status(200).send(JSON.stringify(blockchain.chain));
+});
+
+app.post("/mine", (request, response) => {
+    try {
+        Block.mineBlock(blockchain.chain[blockchain.chain.length - 1], request.body || "");
+        response.status(201).json({ message: "Block mined successfully" });
+    } catch (e) {
+        response.status(400)
+        throw e;
+    }
+})
+
+app.listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+}).on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+});
